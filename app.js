e2eb6ceed6b96baa7a6349f944fa50395fc5963f
Rudy yeh <s3352250zz@gamil.com>
2021-08-30 16:34:54 +0800

    add start script

--- app.js
+++ app.js
@@ -152,6 +152,7 @@ app.use((err, req, res, next) => {
 	res.status(statusCode).render('error', { err });
 });
 
-app.listen(3000, () => {
-	console.log('serving on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+	console.log(`serving on port ${port}`);
 });
